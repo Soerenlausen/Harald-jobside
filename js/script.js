bfa4caeeae9afa95d66ede28fcfd8e09ded40520
CyAim <jonaswedelgreen@gmail.com>
2025-06-10 23:28:51 +0200

    Header og Footer (Mangler noter og optimering)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 'use strict'
+
+const menu = document.getElementById('menu');
+const mobileNav = document.getElementById('mobile-nav');
+
+const toggleNav = () => {
+  mobileNav.style.right = mobileNav.style.right === '0px' ? '-100vw' : '0px';
+};
+
+
+menu.addEventListener('click', toggleNav);
